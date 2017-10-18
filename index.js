a49ece0e99f63499724d7d1db0cc4ad2639b5fa9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 20:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function changeLeastFavoriteCustomer() {
 
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = 'chris'
-  favoriteCustomer = 'jim'
+  let favoriteCustomer = 'jim'
 }
